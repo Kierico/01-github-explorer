90bfb903a540cf925d2f39791c27dd012cc06771
Kiérico Souza <kiericograndao@gmail.com>
2022-05-15 19:57:36 -0400

    3.2_Utilizando-o-useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,20 +1,24 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 
 import "../styles/repositories.scss";
 
-const repository = {
-    name: 'Unform',
-    description: 'Forms in React',
-    link: 'https://github.com/kierico'
-}
+/** https://api.github.com/users/Kierico/repos */
 
 export function RepositoryList() {
+
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/Kierico/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, []);
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
                 <RepositoryItem repository={repository} />
             </ul>
         </section>
