a13e38c8e05c7eac1380a18a0fd98ec06b459561
Kiérico Souza <kiericograndao@gmail.com>
2022-05-15 20:23:46 -0400

    Configuração-ambiente-atualizada

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,8 @@
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
 import { App } from './App';
 
-render(<App />, document.getElementById('root'));
+root.render(<App />);
+
+/** https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis */
